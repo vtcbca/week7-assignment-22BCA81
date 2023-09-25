45ac2e8cbdb2094daaea7b4ae5f47726d8c11257
22BCA81 <122865066+22BCA81@users.noreply.github.com>
2023-09-25 23:15:52 +0530

    Add files via upload

---
+++ Week7/solustion4.py
@@ -0,0 +1,13 @@
+from pandas import *
+
+Column=['Product No', 'Product Name', 'January', 'February', 'March', 'April', 'May', 'June']
+dtf = read_csv('Student_Data.csv')
+data = dtf.values.tolist()
+total=[sum(i[2::]) for i in data]
+average=[int(sum(i[2::])/6) for i in data]
+
+dataframe = DataFrame(data,columns=Column)
+dataframe['Total']=total
+dataframe['Average']=average
+
+print(dataframe)
